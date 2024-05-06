d467a82c20dfbfa75d5770d55460c9a9a8c41853
EmperorNao <ansicpp2020@gmail.com>
2024-05-06 22:33:53 +0300

    Add real pcl test on voxelization

--- ops/test_voxel_op.py
+++ ops/test_voxel_op.py
@@ -1,17 +1,19 @@
 import torch
+import numpy as np
+import matplotlib
+
+import matplotlib.pyplot as plt
 from voxel_op import hard_voxelize
 
 
 def test_voxel_op():
+    
+    points = torch.FloatTensor(np.fromfile("ops/0000000000.bin", dtype=np.float32).reshape(-1, 4))[:, :3]
 
-    points = torch.FloatTensor(
-        [[0.1, 0.0, 0.0], [0.3, 0.3, 0.0], [0.0, -0.1, 0.0], [2.1, 2.1, 1.0], [2.0, 2.0, 1.0]]
-    )
-
-    voxel_size = [1.0, 1.0, 0.2]
-    coors_range = [-2, -2, -2, 2, 2, 2]
-    max_points=2
-    max_voxels=10
+    voxel_size = [1.0, 1.0, 0.5]
+    coors_range = [-50, -50, -4, 50, 50, 4]
+    max_points=10
+    max_voxels=10000
 
     voxels = points.new_zeros(size=(max_voxels, max_points, points.size(1)))
     coors = points.new_zeros(size=(max_voxels, 3), dtype=torch.int)
@@ -20,7 +22,17 @@ def test_voxel_op():
     voxel_num = hard_voxelize(points, voxels, coors,
                                 num_points_per_voxel, voxel_size,
                                 coors_range, max_points, max_voxels, 3)
-    print(voxel_num)
+    
+    img = np.zeros((100, 100))
+
+    for c in coors:
+        img[c[1]][c[2]] = 1
+
+    plt.figure(figsize=(16, 10))
+    plt.scatter(points[:, 0] + 50, points[:, 1] + 50, s=1.0)    
+    plt.imshow(img)
+    plt.show()
+
 
 
 if __name__ == "__main__":
