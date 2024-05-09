2200a3e05cd741c83a0331cf2d9e951577d9f4e7
EmperorNao <ansicpp2020@gmail.com>
2024-05-09 12:20:06 +0300

    Add overlap and iou tests

--- ops/test_iou_op.py
+++ ops/test_iou_op.py
@@ -1,5 +1,39 @@
 import torch
-from iou3d_op import nms_gpu
+from iou3d_op import nms_gpu, boxes_overlap_bev_gpu, boxes_iou_bev_gpu
+
+
+def test_overlap_bev():
+    boxes = torch.Tensor([
+        [0.0, 0.0, 0.5, 0.5, 0.0], 
+        [0.3, 0.0, 0.5, 0.5, 0.0],
+        [0.03, 0.0, 0.5, 0.5, 0.0],
+        [-0.5, 0.0, 0.5, 0.5, 0.0],
+        [0.2, 0.2, 0.5, 0.5, 0.0],
+        [0.25, 0.25, 0.5, 0.5, 0.0]
+        ])
+    
+    ans_overlap = boxes.new_zeros(
+        torch.Size((boxes.shape[0], boxes.shape[0])))
+
+    boxes_overlap_bev_gpu(boxes, boxes, ans_overlap)
+    print(ans_overlap)
+
+
+def test_iou_bev():
+    boxes = torch.Tensor([
+        [0.0, 0.0, 0.5, 0.5, 0.0], 
+        [0.3, 0.0, 0.5, 0.5, 0.0],
+        [0.03, 0.0, 0.5, 0.5, 0.0],
+        [-0.5, 0.0, 0.5, 0.5, 0.0],
+        [0.2, 0.2, 0.5, 0.5, 0.0],
+        [0.25, 0.25, 0.5, 0.5, 0.0]
+        ])
+    
+    ans_overlap = boxes.new_zeros(
+        torch.Size((boxes.shape[0], boxes.shape[0])))
+
+    boxes_iou_bev_gpu(boxes, boxes, ans_overlap)
+    print(ans_overlap)
 
 
 def test_nms_gpu():
@@ -20,3 +54,5 @@ def test_nms_gpu():
 
 if __name__ == "__main__":
     test_nms_gpu()
+    test_iou_bev()
+    test_overlap_bev()
