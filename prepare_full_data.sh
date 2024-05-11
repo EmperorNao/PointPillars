9b4f5cd9a6d14ed950fda8152531d16f7cd3b112
EmperorNao <ansicpp2020@gmail.com>
2024-05-11 17:37:13 +0300

    Change prepare data

--- prepare_full_data.sh
+++ prepare_full_data.sh
@@ -6,5 +6,7 @@ sudo mkdir /data/sets
 ln -s $(pwd)/nuscenes /data/sets/
 mkdir nuscenes_kitti
 python3 -m nuscenes.scripts.export_kitti nuscenes_gt_to_kitti --nusc_kitti_dir nuscenes_kitti
+cp -r nuscenes_kitti/mini_train nuscenes_kitti/training
+cp -r nuscenes_kitti/mini_train nuscenes_kitti/testing
 mkdir dataset/ImageSetsNuscenes
 python3 parse_nuscene_files.py
