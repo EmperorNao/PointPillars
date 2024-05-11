513e4b1b12bee2bfc2394f4f448432e4b24e50b5
EmperorNao <ansicpp2020@gmail.com>
2024-05-11 17:35:29 +0300

    Change prepare data

--- prepare_full_data.sh
+++ prepare_full_data.sh
@@ -2,8 +2,8 @@ wget https://www.nuscenes.org/data/v1.0-mini.tgz
 mkdir nuscenes
 tar -xvf v1.0-mini.tgz -C nuscenes
 sudo mkdir /data
-sudpo mkdir /data/set
-ln -s $(pwd)/nuscenes /data/set/
+sudo mkdir /data/sets
+ln -s $(pwd)/nuscenes /data/sets/
 mkdir nuscenes_kitti
 python3 -m nuscenes.scripts.export_kitti nuscenes_gt_to_kitti --nusc_kitti_dir nuscenes_kitti
 mkdir dataset/ImageSetsNuscenes
