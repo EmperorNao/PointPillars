16d98eef392f57689b3ba763ba2d28d856dd6a65
MMotionMan <114028585+KirichenkoAV22VT191@users.noreply.github.com>
2024-05-18 13:24:51 +0300

    add 160 epoch weights

--- prepare_full_data.sh
+++ prepare_full_data.sh
@@ -2,9 +2,11 @@ wget https://www.nuscenes.org/data/v1.0-mini.tgz
 mkdir nuscenes
 tar -xvf v1.0-mini.tgz -C nuscenes
 sudo mkdir /data
-sudpo mkdir /data/set
-ln -s $(pwd)/nuscenes /data/set/
+sudo mkdir /data/sets
+ln -s $(pwd)/nuscenes /data/sets/
 mkdir nuscenes_kitti
 python3 -m nuscenes.scripts.export_kitti nuscenes_gt_to_kitti --nusc_kitti_dir nuscenes_kitti
+cp -r nuscenes_kitti/mini_train nuscenes_kitti/training
+cp -r nuscenes_kitti/mini_train nuscenes_kitti/testing
 mkdir dataset/ImageSetsNuscenes
 python3 parse_nuscene_files.py
--- train.py
+++ train.py
@@ -37,8 +37,11 @@ def main(args):
 
     if not args.no_cuda:
         pointpillars = PointPillars(nclasses=args.nclasses).cuda()
+        model.load_state_dict(torch.load("pretrained/epoch_160.pth"))
     else:
         pointpillars = PointPillars(nclasses=args.nclasses)
+        model.load_state_dict(
+            torch.load("pretrained/epoch_160.pth", map_location=torch.device('cpu')))
     loss_func = Loss()
 
     max_iters = len(train_dataloader) * args.max_epoch
